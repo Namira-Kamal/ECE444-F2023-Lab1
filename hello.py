4054340d2492664369b3f1133f627e47c1ec797f
namirakamal <itznamira@gmail.com>
2023-12-20 19:01:12 -0500

    Modifying hello.py with chapter 3 changes, adding hmtl templates

--- hello.py
+++ hello.py
@@ -1,8 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
-@app.route('/')
-def index():
- return '<h1>Hello World!</h1>'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.errorhandler(404) 
+def page_not_found(e): 
+    return render_template('404.html'), 404 
+
+@app.errorhandler(500) 
+def internal_server_error(e): 
+    return render_template('500.html'), 500
+
+@app.route('/') 
+def index(): 
+    return render_template('index.html', current_time=datetime.utcnow())
+
 @app.route('/user/<name>')
 def user(name):
- return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
+
+# example 2-1
+# @app.route('/')
+# def index():
+#     return '<h1>Hello World!</h1>'
+
+# example 2-2
+# @app.route('/user/<name>')
+# def user(name):
+#     return '<h1>Hello, {}!</h1>'.format(name)
+
